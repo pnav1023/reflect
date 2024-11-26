a2f17f8ff10a5ff32564875c3f99801356fcbd5c
Pranav Narahari <naraharipranav@gmail.com>
2024-11-26 01:40:44 -0600

    updated gitignore:

--- ui.py
+++ ui.py
@@ -2,6 +2,6 @@ import streamlit as st
 from rag_scripts import get_top_n_similar
 from llm_req import get_chat_bot
 
-st.title('Reflect AI')
+st.title('Reflect AI: Your Personal Reflection Assistant')
 
 get_chat_bot(get_top_n_similar("Places I've been"))
